bc2f338d3b23054e77604fdcbbee8ddb465397b9
Pankaj <pankaj.katkar.16001@iitgoa.ac.in>
2020-09-06 23:16:19 +0530

    Updates

---
+++ precache-manifest.bf0e64215bed77c4fd1085b58bdd3811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26559afa49bf52ddbd9f6c5928a6c9b1",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "a46a3998eebb6bc75738",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9118fb2a13b39277b2be",
+    "url": "/react-deploy/static/js/2.aa5044b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.aa5044b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a46a3998eebb6bc75738",
+    "url": "/react-deploy/static/js/main.e0ca7cb0.chunk.js"
+  },
+  {
+    "revision": "7680564a608065f53872",
+    "url": "/react-deploy/static/js/runtime-main.b5352b27.js"
+  }
+]);
