af651156b9847e3e970048c8c15612789bb5841a
Pankaj <pankaj.katkar.16001@iitgoa.ac.in>
2020-09-06 23:13:40 +0530

    deploy react to github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Helo from './component/hello'
+
+function App() {
+  return (
+    <div>
+      <Helo />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/component/hello.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Helo(){
+    return(
+        <h1>Heloo Pankaj </h1>
+    );
+}
+
+export default Helo
